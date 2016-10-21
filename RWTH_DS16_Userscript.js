7f40c64892641b9684eaeb4d8e333ec1d2adbe31
Paul Sütterlin <psuet@users.noreply.github.com>
2016-10-21 11:19:33 +0200

    Create RWTH_DS16_Userscript.js

---
+++ RWTH_DS16_Userscript.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         RWTH - DS16 Enhancer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  MathJAX FTW
+// @author       Paul Sütterlin
+// @match        https://www2.math.rwth-aachen.de/DS16/exquery.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+document.evaluate("//select[@name='format']",document,null,9,null).singleNodeValue.selectedIndex=1;
+
+})();
