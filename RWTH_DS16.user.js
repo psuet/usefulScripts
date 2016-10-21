ea36c21f49960cdeaf706cd85df821178d54bc2a
Paul Sütterlin <psuet@users.noreply.github.com>
2016-10-21 23:39:37 +0200

    Loggin Notice

--- RWTH_DS16.user.js
+++ RWTH_DS16.user.js
@@ -3,6 +3,7 @@
 // @namespace    https://github.com/psuet/userscripts/
 // @version      0.2
 // @description  MathJAX FTW
+// @description  You have to be a student at RWTH Aachen University to be able to use this userscript
 // @author       Paul Sütterlin
 // @match        https://www2.math.rwth-aachen.de/DS16/exquery.html
 // @updateURL    https://raw.githubusercontent.com/psuet/userscripts/master/RWTH_DS16.user.js
