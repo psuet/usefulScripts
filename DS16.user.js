881afa494bb1ba191ead298bfddbea6a1d44d05d
Paul Sütterlin <psuet@users.noreply.github.com>
2016-11-03 22:52:51 +0100

    Update DS16.user.js

--- DS16.user.js
+++ DS16.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name         RWTH - Diskrete Strukturen WS 16
+// @name         DS16 
 // @namespace    https://github.com/psuet/userscripts/
 // @version      0.21
 // @description  MathJAX FTW
-// @description  You have to be a student at RWTH Aachen University to be able to use this userscript
 // @author       Paul Sütterlin
 // @match        https://www2.math.rwth-aachen.de/DS16/exquery.html
-// @updateURL    https://raw.githubusercontent.com/psuet/userscripts/master/RWTH_DS16.user.js
-// @downloadURL  https://raw.githubusercontent.com/psuet/userscripts/master/RWTH_DS16.user.js
+// @updateURL    https://raw.githubusercontent.com/psuet/userscripts/master/DS16.user.js
+// @downloadURL  https://raw.githubusercontent.com/psuet/userscripts/master/DS16.user.js
 // @grant        none
 // ==/UserScript==
 
