4e509744834a5d885fb1ce64dc1833bc434f0637
Paul Sütterlin <psuet@users.noreply.github.com>
2016-11-03 22:50:42 +0100

    Update

--- L2P.user.js
+++ L2P.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name         RWTH - L2P Enhancer
+// @name         L2P Enhancer
 // @namespace    https://github.com/psuet/userscripts/
-// @version      0.1
+// @version      0.11
 // @description  Autoredirects you to the page "My Courses"
 // @description  You have to be a student at RWTH Aachen University to be able to use this userscript
 // @updateURL    https://raw.githubusercontent.com/psuet/userscripts/master/RWTH_L2P.user.js
 // @downloadURL  https://raw.githubusercontent.com/psuet/userscripts/master/RWTH_L2P.user.js
 // @author       Paul Sütterlin
 // @match        https://www3.elearning.rwth-aachen.de/*
-// @icon         https://www3.elearning.rwth-aachen.de/_layouts/15/L2PAssets/ico/favicon.ico
 // @grant        none
 // ==/UserScript==
 
