f713aa05b1b84836fa112f56ec237ea90d92eb85
Paul Sütterlin <psuet@users.noreply.github.com>
2016-11-03 22:56:42 +0100

    Update DS16.user.js

--- DS16.user.js
+++ DS16.user.js
@@ -5,8 +5,8 @@
 // @description  MathJAX FTW
 // @author       Paul Sütterlin
 // @match        https://www2.math.rwth-aachen.de/DS16/exquery.html
-// @updateURL    https://raw.githubusercontent.com/psuet/userscripts/master/DS16.user.js
-// @downloadURL  https://raw.githubusercontent.com/psuet/userscripts/master/DS16.user.js
+// @updateURL    https://raw.githubusercontent.com/psuet/usefulScripts/master/DS16.user.js
+// @downloadURL  https://raw.githubusercontent.com/psuet/usefulScripts/master/DS16.user.js
 // @grant        none
 // ==/UserScript==
 
