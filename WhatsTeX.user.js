c74ea78b6ffa6fac0f555fe37a026ecbf4fcd809
Paul Sütterlin <psuet@users.noreply.github.com>
2017-01-18 20:45:33 +0100

    Update WhatsTeX.user.js

--- WhatsTeX.user.js
+++ WhatsTeX.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://paulsuetterlin.de
 // @author         Paul Sütterlin
 // @description    LaTeX for WhatsApp
-// @version        3
+// @version        4
 // @downloadURL    https://github.com/ninov/watex/raw/master/watex.user.js
 // @updateURL      https://github.com/ninov/watex/raw/master/watex.user.js
 // @include        https://web.whatsapp.com/
