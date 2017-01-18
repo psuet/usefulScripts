c9a30ac3c3687b9748eab55eb703a83c0fcc1293
Paul Sütterlin <psuet@users.noreply.github.com>
2017-01-18 20:45:14 +0100

    Update WhatsTeX.user.js

--- WhatsTeX.user.js
+++ WhatsTeX.user.js
@@ -5,9 +5,7 @@
 // @description    LaTeX for WhatsApp
 // @version        3
 // @downloadURL    https://github.com/ninov/watex/raw/master/watex.user.js
-// @updateURL      https://github.com/psuet/usefulScripts/raw/master/WhatsTeX.user.js
+// @updateURL      https://github.com/ninov/watex/raw/master/watex.user.js
 // @include        https://web.whatsapp.com/
 // ==/UserScript==
-(function(){
-  alert(“WhatsTex is outdated. Use https://github.com/ninov/watex“);
-  });
+  alert('WhatsTex is outdated. Use https://github.com/ninov/watex');
