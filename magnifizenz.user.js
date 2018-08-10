2d1e75020543296f1eaa271c4e2bc6ce8b896139
Julius Rickert <juliusrickert@users.noreply.github.com>
2018-08-10 19:07:22 +0200

    Update magnifizenz.user.js

--- magnifizenz.user.js
+++ magnifizenz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Schmachtenberg
 // @namespace    https://github.com/psuet/usefulScripts/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Brings back Schmachtenberg to campusOffice
 // @updateURL    https://raw.githubusercontent.com/psuet/usefulScripts/master/magnifizenz.user.js
 // @downloadURL  https://raw.githubusercontent.com/psuet/usefulScripts/master/magnifizenz.user.js
@@ -12,7 +12,7 @@
 // ==/UserScript==
 
 (function() {
-    let img = document.querySelector("img[src=\"system/help/images/magnifizenz.jpg\"]");
-    img.src = "https://web.archive.org/web/20170607122826im_/https://www.campus.rwth-aachen.de/office/system/help/images/magnifizenz.jpg";
-    img.parentElement.parentElement.nextElementSibling.querySelector("td").innerHTML = "Univ.-Prof. Dr.-Ing. Ernst Schmachtenberg<br>Rektor der RWTH Aachen";
+    let img = document.querySelector('img[src="system/help/images/magnifizenz.jpg"]').parentElement;
+    img.innerHTML = '<img src="https://rwth.fun/magnifizenz.jpg" integrity="sha384-V1kwHliOPoD/2tyAsKnujW7pwfF4C3tFKZiOVUVFqQtI6mWRoLa3VKOepe8bBclD">';
+    img.parentElement.nextElementSibling.querySelector('td').innerHTML = 'Univ.-Prof. Dr.-Ing. Ernst Schmachtenberg<br>Rektor der RWTH Aachen';
 })();
